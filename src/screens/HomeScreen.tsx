d85d03990408f9d53268a4e4fe43876a2cebc328
vaibhavgadekar <gadekarv1@gmail.com>
2024-11-06 14:22:43 +0530

    feat: i18n setup

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,32 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {ScrollView, StatusBar, View} from 'react-native';
+import ArrowLeft from '../assets/icons/ArrowLeft';
+import PMHeader from '../components/molecules/PMHeader';
 
 export const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  const goBack = () => {
+    navigation.goBack();
+  };
   return (
     <View>
-      <Text> textInComponent </Text>
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
+      <PMHeader
+        title="हनुमानगढ़ी के इतिहास"
+        leftIcon={{
+          icon: <ArrowLeft />,
+          onPress: goBack,
+        }}
+      />
+      <ScrollView>
+        <View style={{height: 400, width: '100%', backgroundColor: 'white'}} />
+        <View style={{height: 400, width: '100%', backgroundColor: 'pink'}} />
+        <View style={{height: 400, width: '100%', backgroundColor: 'green'}} />
+        <View style={{height: 400, width: '100%', backgroundColor: 'gray'}} />
+        <View style={{height: 400, width: '100%', backgroundColor: 'blue'}} />
+      </ScrollView>
     </View>
   );
 };
