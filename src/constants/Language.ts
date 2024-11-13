be337c4ddac3a15ba171e95526f71eb03d650494
akashgadekar1 <gadekarakashdeepak@gmail.com>
2024-11-12 20:37:24 -0800

    transltion dialogbox create

--- src/constants/Language.ts
+++ src/constants/Language.ts
@@ -7,9 +7,11 @@ export const SupportedLanguages = [
   {
     value: Languages.HI,
     label: 'हिन्दी',
+    shortCode: 'हि'
   },
   {
     value: Languages.EN,
     label: 'English',
+    shortCode: 'A'
   },
 ];
